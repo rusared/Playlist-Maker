3aba5204a076cbd09cb4a73224ace5703553d5aa
rusared <rusared@ya.ru>
2025-02-17 01:13:35 +0500

    Спринт 9. Реализовал кнопки экрана настроек в соответствии с домашним заданием. Все строковые ресурсы выненесены в файл strings.xml.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,25 +10,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val searchButton = findViewById<Button>(R.id.search_button)
-        val libraryButton = findViewById<Button>(R.id.library_button)
-        val settingsButton = findViewById<Button>(R.id.settings_button)
+        val searchButton = findViewById<Button>(R.id.main_search_button)
+        val libraryButton = findViewById<Button>(R.id.main_library_button)
+        val settingsButton = findViewById<Button>(R.id.main_settings_button)
 
-
-        val searchButtonClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                val searchButtonIntent = Intent(this@MainActivity, SearchActivity::class.java)
-                startActivity(searchButtonIntent)
-            }
+        searchButton.setOnClickListener {
+            val searchButtonIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchButtonIntent)
         }
-        searchButton.setOnClickListener(searchButtonClickListener)
-
 
         libraryButton.setOnClickListener {
             val libraryButtonIntent = Intent(this, LibraryActivity::class.java)
             startActivity(libraryButtonIntent)
         }
-
 
         settingsButton.setOnClickListener {
             val settingsButtonIntent = Intent(this, SettingsActivity::class.java)
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,19 +1,56 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textview.MaterialTextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        val backButton = findViewById<MaterialToolbar>(R.id.settings_back_button)
+        setSupportActionBar(backButton)
+        val shareButton = findViewById<MaterialTextView>(R.id.settings_share_button)
+        val supportButton = findViewById<MaterialTextView>(R.id.settings_support_button)
+        val agreementButton = findViewById<MaterialTextView>(R.id.settings_agreement_button)
 
-        toolbar.setNavigationOnClickListener {
+        backButton.setNavigationOnClickListener {
             finish()
         }
+        shareButton.setOnClickListener {
+            shareApp()
+        }
+        supportButton.setOnClickListener {
+            mailToSupport()
+        }
+        agreementButton.setOnClickListener {
+            showUserAgreement()
+        }
+    }
+
+    private fun shareApp() {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.app_url))
+        startActivity(Intent.createChooser(intent, resources.getString(R.string.share_via)))
+    }
+
+    private fun mailToSupport() {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.data = Uri.parse("mailto:")
+        intent.putExtra(Intent.EXTRA_EMAIL, resources.getString(R.string.support_email))
+        intent.putExtra(Intent.EXTRA_SUBJECT, resources.getString(R.string.support_mail_subject))
+        intent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.support_mail_text))
+        startActivity(intent)
+    }
+
+    private fun showUserAgreement() {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(resources.getString(R.string.user_agreement))
+        startActivity(intent)
     }
 }
